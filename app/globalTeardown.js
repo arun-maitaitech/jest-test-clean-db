3d5e5ca0a0d2ac5396e7dcf0562a3af030fd145d
Asaf <AsafShifer@Gmail.com>
2024-05-01 02:43:18 +0300

    .

---
+++ app/globalTeardown.js
@@ -0,0 +1,3 @@
+const afterAll = require('./dist/afterAll');
+exports.default = afterAll.default;
+module.exports = afterAll.default;
--- demo/jest.config.js
+++ demo/jest.config.js
@@ -69,7 +69,7 @@ module.exports = {
   // globalSetup: './jest.beforeAll.ts',
 
   // A path to a module which exports an async function that is triggered once after all test suites
-  globalTeardown:  'jest-test-clean-db/dist/globalTeardown',
+  globalTeardown:  'jest-test-clean-db/globalTeardown',
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
@@ -155,7 +155,7 @@ module.exports = {
   setupFiles: ['dotenv/config'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['jest-test-clean-db/dist/setupFilesAfterEnv'],
+  setupFilesAfterEnv: ['jest-test-clean-db/setupFilesAfterEnv'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
