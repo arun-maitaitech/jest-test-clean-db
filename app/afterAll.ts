8927e7434e939f9db7ed321ca037759c4823547e
Asaf <AsafShifer@Gmail.com>
2024-08-06 22:29:11 +0300

    .

--- app/afterAll.ts
+++ app/afterAll.ts
@@ -2,8 +2,8 @@ import { deleteDb } from './dbRelatedFunctions';
 import { getMainDataSource, wasEverInitialized } from './mainDataSource_singleton';
 import { getTemplateDbName } from './templateDbNameGenerator_singleton';
 
-// export default async () => {
-module.exports = async function () {
+export default async () => {
+// module.exports = async function () {
   const templateDbName = getTemplateDbName();
   if (wasEverInitialized() && templateDbName) {
     const mainDataSource = await getMainDataSource();
